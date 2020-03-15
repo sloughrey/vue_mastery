4a6b5d606da9a2b49ad16300ffe45a9f80820d29
Sean Loughrey <s.a.loughrey@gmail.com>
2020-03-15 19:45:24 -0400

    Created an event bus out of a Vue instance to fire  events throughout the application. Used the 'mounted' lifecycle hook to trigger the event to add a review to the product array (grandparent component). Created a 'tabs' component to switch between reading / writing reviews. Completed the vue into course.

--- 1_intro_to_vue_js/main.js
+++ 1_intro_to_vue_js/main.js
@@ -1,3 +1,5 @@
+var eventBus = new Vue();
+
 Vue.component('product', {
     props: {
         premium: {
@@ -52,19 +54,7 @@ Vue.component('product', {
                 <li v-for="size in sizes">{{ size }}</li>
             </ul>
 
-            <div>
-                <h2>Reviews</h2>
-                <p v-if="!reviews.length">There are no reviews yet.</p>
-                <ul>
-                    <li v-for="review in reviews">
-                        <p>{{ review.name }}</p>
-                        <p>Rating: {{ review.rating }}</p>
-                        <p>{{ review.review }}</p>
-                    </li>
-                </ul>
-            </div>
-
-            <product-review @review-submitted="addReview"></product-review>
+            <product-tabs :reviews="reviews"></product-tabs>
         </div>
     `,
     data() {
@@ -111,9 +101,6 @@ Vue.component('product', {
         isOnSale() {
             return this.getSelectedVariant().variantOnSale;
         },
-        addReview(productReview) {
-            this.reviews.push(productReview);
-        }
     },
     computed: { // computed properties are cached
         title() {
@@ -141,6 +128,11 @@ Vue.component('product', {
             }
             return 2.99;
         }
+    },
+    mounted() { // lifecycle hook code runs aftert component is mounted to the dom
+        eventBus.$on('review-submitted', productReview => { // listens for the review-submitted event to fire
+            this.reviews.push(productReview);
+        });
     }
 });
 
@@ -213,7 +205,7 @@ Vue.component('product-review', {
                     rating: this.rating,
                     recommend: this.recommend,
                 }
-                this.$emit('review-submitted', productReview);
+                eventBus.$emit('review-submitted', productReview);
                 this.name = null;
                 this.review = null;
                 this.rating = null;
@@ -242,6 +234,52 @@ Vue.component('productDetails', {
     `,
 });
 
+Vue.component('product-tabs', {
+    props: {
+        reviews: {
+            type: Array,
+            required: true
+        }
+    },
+    template: `
+        <div>
+            <span class="tab"
+                :class="{ activeTab: selectedTab == tab}"
+                v-for="(tab, index) in tabs"
+                :key="index"
+                @click="selectedTab = tab">
+                {{ tab }}
+            </span>
+
+
+            <div>
+                <h2>Reviews</h2>
+                <div v-show="selectedTab === 'Reviews'">
+                    <p v-if="!reviews.length">There are no reviews yet.</p>
+                    <ul>
+                        <li v-for="review in reviews">
+                            <p>{{ review.name }}</p>
+                            <p>Rating: {{ review.rating }}</p>
+                            <p>{{ review.review }}</p>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+
+            <product-review v-show="selectedTab === 'Make a Review'"></product-review>
+
+        </div>
+    `,
+    data() {
+        return {
+            tabs: [
+                'Reviews',
+                'Make a Review'
+            ],
+            selectedTab: 'Reviews',
+        }
+    }
+});
 
 var app = new Vue({
     el: '#app',
