8e90dd8534128b572cff017803f7c1acb18bb017
Sean Loughrey <s.a.loughrey@gmail.com>
2020-04-03 16:37:12 -0400

    Adding missing files from previous commit

---
+++ .prettierrc.js
@@ -0,0 +1,5 @@
+module.exports = {
+  singleQuote: true,
+  semi: false,
+  htmlWhitespaceSensitivity: 'ignore' // this helps so it doesn't break opening/closing tags onto new lines in vue files
+}
---
+++ 2_real_world_vue/src/views/EventList.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <h1>Events Listing</h1>
+    <EventCard />
+  </div>
+</template>
+
+<script>
+import EventCard from '@/components/EventCard.vue'
+
+// register child components for our template to use
+export default {
+  components: {
+    EventCard
+  }
+}
+</script>
